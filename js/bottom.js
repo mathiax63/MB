3972170f4f850482c5586e261353e09a22eed71d
mathiax63 <mathyoyo@hotmail.es>
2022-01-23 17:54:27 -0300

    suboJS

--- js/bottom.js
+++ js/bottom.js
@@ -1,10 +1,117 @@
-window.addEventListener("load",function(){
+
+/*window.addEventListener("load", function () {})*/
+
+function bars(){
+let bar = document.getElementById("botton")
+bar.style.color = "Gray"
+
+}
+
+function bars2(){
+let bar2 = document.getElementById("botton2")
+bar2.style.color = "Gray"
+}
+
+function baroout (){
+    let bar = document.getElementById("botton")
+    bar.style.color = "black"
+}
+function baroout2 (){
+    let bar2 = document.getElementById("botton2")
+    bar2.style.color = "black"
+}
 
 
-
-
-
-
-
+function imgsP (){
+    let p = document.getElementById("idP")
+    p.style.display = "block"
     
-})
+}
+function imgsPOut (){
+    let p = document.getElementById("idP")
+    p.style.display = "none"
+    
+}
+
+function imgsP2 (){
+    let p = document.getElementById("idP2")
+    p.style.display = "block"
+    
+}
+function imgsP3 (){
+    let p = document.getElementById("idP3")
+    p.style.display = "block"
+    
+}
+function imgsP4 (){
+    let p = document.getElementById("idP4")
+    p.style.display = "block"
+    
+}
+function imgsP5 (){
+    let p = document.getElementById("idP5")
+    p.style.display = "block"
+    
+}
+function imgsP6 (){
+    let p = document.getElementById("idP6")
+    p.style.display = "block"
+    
+}
+function imgsP7 (){
+    let p = document.getElementById("idP7")
+    p.style.display = "block"
+    
+}
+function imgsP8 (){
+    let p = document.getElementById("idP8")
+    p.style.display = "block"
+    
+}
+function imgsP9 (){
+    let p = document.getElementById("idP9")
+    p.style.display = "block"
+    
+}
+
+
+function imgsPOut2 (){
+    let p = document.getElementById("idP2")
+    p.style.display = "none"
+    
+}
+function imgsPOut3 (){
+    let p = document.getElementById("idP3")
+    p.style.display = "none"
+    
+}
+function imgsPOut4 (){
+    let p = document.getElementById("idP4")
+    p.style.display = "none"
+    
+}
+function imgsPOut5 (){
+    let p = document.getElementById("idP5")
+    p.style.display = "none"
+    
+}
+function imgsPOut6 (){
+    let p = document.getElementById("idP6")
+    p.style.display = "none"
+    
+}
+function imgsPOut7 (){
+    let p = document.getElementById("idP7")
+    p.style.display = "none"
+    
+}
+function imgsPOut8 (){
+    let p = document.getElementById("idP8")
+    p.style.display = "none"
+    
+}
+function imgsPOut9 (){
+    let p = document.getElementById("idP9")
+    p.style.display = "none"
+    
+}
